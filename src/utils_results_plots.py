5e7b5c9585d0920354abb78326763f82ce0e24fb
pklesk <pklesk@wi.zut.edu.pl>
2026-01-25 12:16:35 +0100

    speedup plots refreshed

--- src/utils_results_plots.py
+++ src/utils_results_plots.py
@@ -7,20 +7,20 @@ if __name__ == "__main__":
     device_names = ["i7-10700@2.90GHz", "RTX 3090", "RTX 5090 Blackwell"]
     
     series = [
-        {"sfwf_contraction_cpu_numpy": [0.037, 0.383, 7.736, 112.205, 4113.502]},
+        {"sfwf_contraction_cpu_numpy": [0.036, 0.390, 8.066, 114.985, 4281.633]},
         {        
-            "sfwf_contraction_cuda_large_atomicmax": [0.074, 0.463, 2.127, 2.077, 22.485],
-            "sfwf_contraction_cuda_large_atomicmaxglosten": [0.074, 0.464, 2.127, 2.086, 18.999],
-            "sfwf_contraction_cuda_large_hreducemax": [0.044, 0.278, 1.276, 1.243, 22.801],
-            "sfwf_contraction_cuda_large_hreducemaxgs": [0.045, 0.282, 1.293, 1.256, 22.936],
-            "sfwf_contraction_cuda_large_gridsync": [0.008, 0.052, 0.756, 4.337, 80.072]
+            "sfwf_contraction_cuda_large_atomicmax": [0.072, 0.469, 2.224, 2.154, 22.430],
+            "sfwf_contraction_cuda_large_atomicmaxglosten": [0.072, 0.469, 2.214, 2.158, 18.961],
+            "sfwf_contraction_cuda_large_hreducemax": [0.043, 0.283, 1.332, 1.297, 22.808],
+            "sfwf_contraction_cuda_large_hreducemaxgs": [0.044, 0.284, 1.334, 1.306, 22.956],
+            "sfwf_contraction_cuda_large_gridsync": [0.008, 0.053, 0.765, 4.301, 79.929]
         },
         {
-            "sfwf_contraction_cuda_large_atomicmax": [0.025, 0.159, 0.725, 0.724, 5.552],
-            "sfwf_contraction_cuda_large_atomicmaxglosten": [0.025, 0.160, 0.724, 0.723, 5.717],
-            "sfwf_contraction_cuda_large_hreducemax": [0.016, 0.100, 0.456, 0.448, 5.321],
-            "sfwf_contraction_cuda_large_hreducemaxgs": [0.016, 0.101, 0.456, 0.452, 5.336],
-            "sfwf_contraction_cuda_large_gridsync": [0.006, 0.042, 0.350, 0.960, 12.146]            
+            "sfwf_contraction_cuda_large_atomicmax": [0.025, 0.160, 0.739, 0.722, 5.552],
+            "sfwf_contraction_cuda_large_atomicmaxglosten": [0.025, 0.161, 0.730, 0.724, 5.718],
+            "sfwf_contraction_cuda_large_hreducemax": [0.016, 0.101, 0.456, 0.448, 5.323],
+            "sfwf_contraction_cuda_large_hreducemaxgs": [0.016, 0.102, 0.460, 0.451, 5.337],
+            "sfwf_contraction_cuda_large_gridsync": [0.006, 0.042, 0.351, 0.960, 12.152]            
         }
     ]
 
